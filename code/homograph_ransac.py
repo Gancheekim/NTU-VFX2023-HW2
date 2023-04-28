3d25bb773073c8adc57606f0b3be6e380789ab87
David Akas <r11942163@ntu.edu.tw>
2023-04-28 21:54:44 +0800

    ransac completed, draw matches in image, compute homograph error done

--- code/homograph_ransac.py
+++ code/homograph_ransac.py
@@ -33,27 +33,33 @@ def filter_matches(kp_left, kp_right, init_matches, threshold):
 		# Return in array format
 		return good, matches_list
 
-# REFERENCE
-def compute_error(homograph, matches):
-    num_points = len(matches)     # number of matches
-    all_p1 = np.concatenate((matches[:, 0:2], np.ones((num_points, 1))), axis=1)
-    all_p2 = matches[:, 2:4]
-    estimate_p2 = np.zeros((num_points, 2))
-    for i in range(num_points):
-        temp = np.dot(homograph, all_p1[i])
-        estimate_p2[i] = (temp/temp[2])[0:2] # set index 2 to 1 and slice the index 0, 1
-    # Compute error
-    errors = np.linalg.norm(all_p2 - estimate_p2 , axis=1) ** 2
 
-    return errors
+def compute_error(homograph, matches):
+	num_matches = len(matches)    # number of matches e.g 229
+	
+	# Separate points from left image, and points from right image
+	U = np.array([list(m[ :2]) + [1] for m in matches]) # U is the set of points u in the left image; append 1 to the back for homogeneity
+	V = np.array([list(m[2: ]) for m in matches]) 		# V is the set of points v in the left image
+
+	# Estimate of a points V in second image, using homograph and U
+	est_V = np.zeros(shape=(num_matches, 2))  # there are 'number of matches' of v, each point has 2 coordinate values
+	for i in range(num_matches):
+		dot_result = np.dot(homograph, U[i])  # numpy dot product
+		v          = (dot_result/dot_result[2])[:2]   # get the two coordinate values for v
+		est_V[i]   = v.copy()
+	
+	# Compute Error (V vs. estimated V)
+	error = np.linalg.norm(V - est_V, axis=1)
+	return error
+
+	
 
 
 # Solve homograph
-def get_homograph_ransac(matches, num_iters=2000, num_samples=4, threshold=0.5):
-	# print(matches)  					 		     # [[299.0, 162.0, 51.0, 160.0], [291.0, 191.0, 43.0, 186.0], [319.0, 194.0, 78.0, 175.0], [370.0, 170.0, 121.0, 168.0]]
-	matches_arr = np.array(matches)
-	num_best_inliers = 0  # score to maximize
-
+def get_homograph_ransac(matches, num_iters=1500, num_samples=4, threshold=0.5):		 		     
+	matches_arr  = np.array(matches)    # [[299.0, 162.0, 51.0, 160.0], [291.0, 191.0, 43.0, 186.0], [319.0, 194.0, 78.0, 175.0], [370.0, 170.0, 121.0, 168.0]]
+	best_score   = 0  				    # number of inliers matches, the score to maximize by RANSAC
+	best_inliers = []                	# inlier matches record
 	# Do ransac iteration
 	for i in range(num_iters):
 		#-------------------------------------------------------------------------------
@@ -65,76 +71,98 @@ def get_homograph_ransac(matches, num_iters=2000, num_samples=4, threshold=0.5):
 		#-------------------------------------------------------------------------------
 		# Step 2: Determine homography of two images, based on sampled points
 		#-------------------------------------------------------------------------------
-		M = []    # matrix representation to do SVD
-		for match_pair in sample_matches:
-			p1 = match_pair[:2] + [1]    # e.g [370.0, 170.0, 1]  
-			p2 = match_pair[2:] + [1]	 # e.g [121.0, 168.0, 1]
-			
-			# Construct Matrix
-			row1 = [0, 0, 0, p1[0], p1[1], p1[2], -p2[1]*p1[0], -p2[1]*p1[1], -p2[1]*p1[2]]    # [0, 0, 0, 370.0, 170.0, 1, -62160.0, -28560.0, -168.0]
-			row2 = [p1[0], p1[1], p1[2], 0, 0, 0, -p2[0]*p1[0], -p2[0]*p1[1], -p2[0]*p1[2]]    # [370.0, 170.0, 1, 0, 0, 0, -44770.0, -20570.0, -121.0]
-			M.append(row1)
-			M.append(row2)
+		# Construct Matrix to Solve by SVD
+		M = np.zeros(shape=(num_samples*2, 9))   # matrix intialization, num_samples*2 rows, 3x3 values in each row
+		for match_idx, match_pair in enumerate(sample_matches):
+			# Identify the coordinates of two points matched
+			u, v = match_pair[:2], match_pair[2:]     # e.g [370.0, 170.0]  (y1,x1) from image 1, [121.0, 168.0]  (y2,x2) from image 2
 
-			# print(f"p1:\n{p1}\n")
-			# print(f"p2:\n{p2}\n")
-			# print(f"row1:\n{row1}\n")
-			# print(f"row2:\n{row2}\n")
-			# print(M)
+			# Sub Matrix (every match pair will contribute 2 rows to the matrix)
+			sub_matrix = [
+				[u[0], u[1], 1, 0, 0, 0, -u[0]*v[0], -u[1]*v[0], -1*v[0]],    # [370.0, 170.0, 1, 0, 0, 0, -44770.0, -20570.0, -121.0]
+				[0, 0, 0, u[0], u[1], 1, -u[0]*v[1], -u[1]*v[1], -1*v[1]],    # [0, 0, 0, 370.0, 170.0, 1, -62160.0, -28560.0, -168.0]
+			]
+
+			# Fill in the matrix with sub matrices
+			M[match_idx*2]   = sub_matrix[0]    # fill in matrix rows 0,2,4,6
+			M[match_idx*2+1] = sub_matrix[1]    # fill in matrix rows 1,3,5,7
 
 		# Solve Matrix
-		M = np.array(M)
-		U, S, Vh = np.linalg.svd(M)
-		homograph = Vh[-1].reshape(3, 3)
-		homograph = homograph / homograph[2,2]  # normalize
+		U, S, Vh = np.linalg.svd(M)			    # perform single value decomposition
+		homograph = Vh[-1].reshape(3, 3)        # obtain a 9x9 matrix
+		homograph = homograph / homograph[2,2]  # normalize to satisfy homogeneity
 		#print(f"homograph:\n{homograph}")
 
 		#-------------------------------------------------------------------------------
 		# Step 3: 
 		#-------------------------------------------------------------------------------
 		#  Skip iteration if rank < 3
-		if np.linalg.matrix_rank(homograph) < 3:
-			continue
+		if np.linalg.matrix_rank(homograph) < 3: continue
 
-        # Find matches with errors < threshold   
-		errors = compute_error(homograph, matches_arr)
-		best_idx = np.where(errors < threshold)[0]    # threshold = 0.75
+        # Find matches with errors < threshold, these are the inliers
+		error = compute_error(
+			homograph = homograph,
+			matches   = matches_arr,
+			)
+
+		best_idx = np.where(error < threshold)[0]    # get indeces, default threshold = 0.6
 		inliers = matches_arr[best_idx]
 		#print(f"errors:\n{errors}")
 
 		# Record best iteration results
-		num_inliers = len(inliers)
-		if num_inliers > num_best_inliers:
-			best_inliers = inliers.copy()
-			num_best_inliers = num_inliers
-			best_H = homograph.copy()
+		score = len(inliers)					      # score for current iteration of RANSAC algorithm
+		if score > best_score:						  # if new best found 
+			best_score     = score				      # update score
+			best_inliers   = inliers				  # update inliers recorded
+			best_homograph = homograph				  # update homograph recorded
 
-	print("inliers/matches: {}/{}".format(num_best_inliers, len(matches)))
+	return best_score, best_inliers, best_homograph
+
+def draw_matches(img_left, img_right, matches):
+	total_img = cv2.hconcat([img_left, img_right])
+	W = total_img.shape[1]  					 		# since we concatenated the image, we should add offset to the x-coordinates
+	offset = W/2								 		# offset in x-direction
 	
-	return best_inliers, best_H
+	for i, m in enumerate(matches):
+		color = (255, 255, 0) 											# in current colorspace, this is light blue
+		u, v = (int(m[0]), int(m[1])), (int(m[2] + offset), int(m[3]))  # coordinates of two points in this match
+		# Draw line
+		cv2.line(
+			img = total_img,
+			pt1 = u,
+			pt2 = v,
+			color = color,  
+			thickness = 1,
+		)
+		# Draw markers
+		cv2.drawMarker(
+			img = total_img,
+			position = u,
+			color = color,
+			markerType = cv2.MARKER_SQUARE,
+			markerSize = 10,
+			thickness = 1,
+			line_type = 8,	
+		)
+		cv2.drawMarker(
+			img = total_img,
+			position = v,
+			color = color,
+			markerType = cv2.MARKER_SQUARE,
+			markerSize = 10,
+			thickness = 1,
+			line_type = 8,	
+		)
 
+	return total_img
 
-
-def plot_matches(matches, img_left, img_right):
-    total_img = np.concatenate((img_left, img_right), axis=1)
-    offset = total_img.shape[1]/2
-    fig, ax = plt.subplots()
-    ax.set_aspect('equal')
-    ax.imshow(np.array(total_img).astype('uint8')) #　RGB is integer type
-    
-    ax.plot(matches[:, 0], matches[:, 1], 'xr')
-    ax.plot(matches[:, 2] + offset, matches[:, 3], 'xr')
-     
-    ax.plot([matches[:, 0], matches[:, 2] + offset], [matches[:, 1], matches[:, 3]], 'r', linewidth=0.5)
-
-    plt.savefig("./../data/output/ransac_output.png")
 
 if __name__ == "__main__":
 	from feature_detection import harris_corner_detection
 
 	# Read images
-	test_img_name_right = "./../data/parrington/prtn01.jpg"  # "./../data/denny/denny01.jpg"
-	test_img_name_left  = "./../data/parrington/prtn02.jpg"  # "./../data/denny/denny00.jpg"
+	test_img_name_right = "./../data/parrington/prtn12.jpg"  # "./../data/denny/denny01.jpg"
+	test_img_name_left  = "./../data/parrington/prtn13.jpg"  # "./../data/denny/denny00.jpg"
 	img_right = cylindrical_proj(cv2.imread(test_img_name_right), 704.676)
 	img_left  = cylindrical_proj(cv2.imread(test_img_name_left), 704.676)
 	# img_right = cv2.imread(test_img_name_right)
@@ -152,9 +180,9 @@ if __name__ == "__main__":
 	cv_kp_left  = coord_to_keypoints(coord_left)
 
 	# Use SIFT
-	sift = SIFT_descriptor()										   							# declare SIFT class object
-	des_right = sift.cal_descriptor(img_right, coord_right)			   							# get descriptor for right image
-	des_left  = sift.cal_descriptor(img_left, coord_left)			   							# get descriptor for left image
+	sift = SIFT_descriptor()								 # declare SIFT class object
+	des_right = sift.cal_descriptor(img_right, coord_right)	 # get descriptor for right image
+	des_left  = sift.cal_descriptor(img_left, coord_left)	 # get descriptor for left image
 
 	#==================
 	# OpenCV Method
@@ -162,21 +190,26 @@ if __name__ == "__main__":
 	'''
 	Comment the following 3 lines to use our method
 	'''
-	# siftDetector= cv2.SIFT_create()
-	# cv_kp_left, des_left = siftDetector.detectAndCompute(img_left, None)
-	# cv_kp_right, des_right = siftDetector.detectAndCompute(img_right, None)
+	siftDetector= cv2.SIFT_create()
+	cv_kp_left, des_left = siftDetector.detectAndCompute(img_left, None)
+	cv_kp_right, des_right = siftDetector.detectAndCompute(img_right, None)
 
 
 	# Basic filtering out match outliers
 	init_matches = sift.find_matches(des_left, des_right)    		   						    # get initial matches by BruteForce: 57 matches
-	good, matches_list = filter_matches(cv_kp_left, cv_kp_right, init_matches, threshold=0.9)   # get good matches
+	good, matches_list = filter_matches(cv_kp_left, cv_kp_right, init_matches, threshold=0.7)   # get good matches
 
 	# Draw Match Results: cv.drawMatchesKnn expects list of lists as matches.
+	# Precleaning of outlier matches
 	img3 = cv2.drawMatchesKnn(img_left, cv_kp_left, img_right, cv_kp_right, good, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 	cv2.imwrite("./../data/output/matched_precleaned.jpg", img3)
 
 	# RANSAC
-	best_inliers, best_homograph = get_homograph_ransac(matches_list)
-	print(matches_list)
-	print(best_inliers)
-	plot_matches(best_inliers, img_left, img_right)
+	best_score, best_inliers, best_homograph = get_homograph_ransac(
+		matches     = matches_list,
+		num_iters   = 1,
+		num_samples = 4,
+		threshold   = 0.5
+		)
+	img4 = draw_matches(img_left, img_right, best_inliers)
+	cv2.imwrite("./../data/output/ransac_output.jpg", img4)
